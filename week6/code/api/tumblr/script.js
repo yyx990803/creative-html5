5fb83377217f4362c9b73dc5c6161f73d1b40801
Evan You <yyx990803@gmail.com>
2013-03-10 00:01:29 -0500

    minor addition

--- week6/code/api/tumblr/script.js
+++ week6/code/api/tumblr/script.js
@@ -10,16 +10,16 @@ var key = 'fuiKNFp9vQFvjLNvx4sUwti4Yb5yGutBN4Xh10LXZhhRKjWlV4'
 
 var url = buildURL('bacac603', key)
 
-$.getJSON(url, function (data) {
+$(function () {
+    $.getJSON(url, function (data) {
 
-    console.log(data)
+        // inspect this in console to see what data we got
+        console.log(data)
 
-    var blog = data.response.blog,
-        posts = data.response.posts
+        createHeader(data.response.blog)
+        createPosts(data.response.posts)
 
-    createHeader(blog)
-    createPosts(posts)
-
+    })
 })
 
 function createHeader (blog) {
@@ -28,17 +28,32 @@ function createHeader (blog) {
 }
 
 function createPosts (posts) {
+    
     posts.forEach(function (post) {
 
         var postElement = $('<div class="post"></div>')
+        postElement.addClass(post.type)
 
+        // you will need to check for different types of posts
+        // and treat them differently
+        // because they will have different properties
         if (post.type === 'photo') {
+
             var imgURL = post.photos[0].original_size.url
             var img = new Image()
             img.src = imgURL
             img.onload = function () {
                 postElement.append(img)
             }
+
+        } else if (post.type === 'text') {
+
+            postElement.append('<h2>' + post.title + '</h2><p>' + post.slug + '</p>')
+
+        } else if (post.type === 'quote') {
+
+            postElement.append('<h3>"' + post.text + '"</h3>')
+
         }
 
         postElement.appendTo('body')
