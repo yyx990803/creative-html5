b2e54e2ac9ba2e9e10ff1f7688034b6c3d397b50
Evan You <yyx990803@gmail.com>
2013-03-21 21:25:18 -0400

    example api

--- week8/api/api.js
+++ week8/api/api.js
@@ -1,16 +1,42 @@
 var http = require('http'),
     fs   = require('fs')
 
+var citiesDataFile = fs.readFileSync('data/cities.json', 'utf-8'),
+    citiesData = JSON.parse(citiesDataFile)
+
+var characterFile = fs.readFileSync('data/characters.json', 'utf-8'),
+    characterData = JSON.parse(characterFile)
+
 var routes = {
 
     // the character method
-    character: function (name) {
+    characters: function (name) {
 
-        var data = {
-            name: 'You gave me: ' + name
+        if (name === 'all') {
+            return characterData
         }
 
-        return data
+        for (var i = 0; i < characterData.length; i++) {
+            var character = characterData[i]
+            if (character.name === name) {
+                return character
+            }
+        }
+
+    },
+
+    cities: function (name) {
+
+        if (name === 'all') {
+            return citiesData
+        }
+
+        for (var i = 0; i < citiesData.length; i++) {
+            var city = citiesData[i]
+            if (city.name === name) {
+                return city
+            }
+        }
 
     }
 
@@ -25,7 +51,7 @@ var server = http.createServer(function (request, response) {
     if (secondSlashIndex === -1) {
         // return 404
         response.end('404')
-        
+
     } else {
 
         var route = request.url.slice(1, secondSlashIndex)
@@ -34,9 +60,13 @@ var server = http.createServer(function (request, response) {
         // if route == 'character'
         // this line basically means
         // routes.character(parameter)
-        var data = routes[route](parameter)
+        if (routes[route]) {
+            var data = routes[route](parameter)
+            response.end(JSON.stringify(data))
+        } else {
+            response.end('404')
+        }
 
-        response.end(JSON.stringify(data))
     }
 
 })
